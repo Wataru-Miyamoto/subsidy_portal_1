a26748ee6641994d6d08a1e1b36bba9b9c004b59
WMUT <WMUT.tech@gmail.com>
2021-08-22 21:54:09 +0900

    draftsを作成

---
+++ app/controllers/drafts_controller.rb
@@ -0,0 +1,21 @@
+class DraftsController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    @draft = Draft.new
+  end
+
+  def create
+
+  end
+
+  def edit
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
-  get 'users/index'
+
   root 'static_pages#top'
   get  '/signup', to: 'users#new'
 
