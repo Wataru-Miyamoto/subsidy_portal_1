876c9d4920b3d5f6d9a1ea88d4025a6a903d27c2
WMUT <WMUT.tech@gmail.com>
2021-09-04 23:00:53 +0900

    市区町村をインポート

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,11 +2,12 @@ class CitiesController < ApplicationController
 
   def index
     @pref = Pref.find(params[:pref_id])
-    @cities = City.where(pref_id: @city.id)
+    @cities = City.where(pref_id: @pref.id)
   end
 
   def import
+    pref = Pref.find(params[:pref_id])
     City.import(params[:file])
-    redirect_to prefs_url
+    redirect_to pref_cities_url(pref)
   end
 end
